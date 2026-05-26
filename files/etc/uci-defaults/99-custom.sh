887e86ce2fdcd018bd0752c456b2cd643937529e
mayi5147 <92247557+mayi5147@users.noreply.github.com>
2026-05-26 13:31:37 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -14,6 +14,9 @@ uci add dhcp domain
 uci set "dhcp.@domain[-1].name=time.android.com"
 uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
+# 编辑默认的luci显示的主机名称
+sed -i 's/ImmortalWrt/EZProV5/g' package/base-files/files/bin/config_generate
+
 # 检查配置文件pppoe-settings是否存在 该文件由build.sh动态生成
 SETTINGS_FILE="/etc/config/pppoe-settings"
 if [ ! -f "$SETTINGS_FILE" ]; then
